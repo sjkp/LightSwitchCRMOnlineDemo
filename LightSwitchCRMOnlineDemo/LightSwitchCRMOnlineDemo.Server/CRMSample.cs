49bb6718084f9f7e8f104fb49dc1c0afebc012fa
Simon Pedersen <mail@sjkp.dk>
2014-09-16 07:12:10 +0200

    missing files

---
+++ LightSwitchCRMOnlineDemo/LightSwitchCRMOnlineDemo.Server/CRMSample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Office365.OAuth;
+
+namespace LightSwitchApplication
+{
+    /// <summary>
+    /// CRMSample class showing how to connect to CRM online by using the Office 365 API tools to get access token. 
+    /// </summary>
+    public class CRMSample
+    {
+        DiscoveryContext _discoveryContext;
+
+        private static Uri ServiceEndpointUri = new Uri(
+            "https://sjkpdev07.crm4.dynamics.com/XRMServices/2011/OrganizationData.svc/");
+
+        private static string ServiceResourceId = "Microsoft.CRM";
+
+        public async Task<string> GetAccessToken()
+        {
+            
+            var client = await EnsureClientCreated();
+            return await client.GetAccessToken();
+        }
+
+
+        private async Task<CRMClient> EnsureClientCreated()
+        {
+            if (_discoveryContext == null)
+            {
+                _discoveryContext = await DiscoveryContext.CreateAsync();
+            }
+
+            var dcr = await _discoveryContext.DiscoverResourceAsync(ServiceResourceId);
+
+            var user = dcr.UserId;
+            return new CRMClient(ServiceEndpointUri, async () =>
+            {
+                var accesToken = (await _discoveryContext.AuthenticationContext.AcquireTokenByRefreshTokenAsync(new SessionCache().Read("RefreshToken"), new Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential(_discoveryContext.AppIdentity.ClientId, _discoveryContext.AppIdentity.ClientSecret), ServiceResourceId)).AccessToken;
+
+                return accesToken;
+            });
+        }
+    }
+}
---
+++ LightSwitchCRMOnlineDemo/LightSwitchCRMOnlineDemo.Server/sjkpdev07Context.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Services.Client;
+using System.Web;
+using LightSwitchApplication;
+using Microsoft.Office365.OAuth;
+
+namespace sjkpdev07ContextData.sjkpdev07ContextDataService
+{
+    public partial class sjkpdev07Context : global::System.Data.Services.Client.DataServiceContext
+    {
+        partial void OnContextCreated()
+        {
+            this.SendingRequest2 += OnSendingRequest2;
+        }
+
+        private void OnSendingRequest2(object sender, SendingRequest2EventArgs sendingRequest2EventArgs)
+        {
+            var token = new SessionCache().Read("AccessToken#Microsoft.CRM");
+            sendingRequest2EventArgs.RequestMessage.SetHeader("Authorization", "Bearer " + token);
+
+        }
+    }
+}
